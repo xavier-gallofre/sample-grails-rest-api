e26821d0338a4ea50970d4725525d074b1e8374d
Xavier <xavier@virtualsw.com>
2019-12-26 12:21:34 +0100

    Application structure changed to a multi project application

--- app/grails-app/controllers/samplegrailsrestapi/ApplicationController.groovy
+++ app/grails-app/controllers/samplegrailsrestapi/ApplicationController.groovy
@@ -1,7 +1,8 @@
 package samplegrailsrestapi
 
 import grails.core.GrailsApplication
-import grails.plugins.*
+import grails.plugins.GrailsPluginManager
+import grails.plugins.PluginManagerAware
 
 class ApplicationController implements PluginManagerAware {
 
